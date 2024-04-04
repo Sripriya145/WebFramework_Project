188baf283fb4332e14c7d3c303380c7d5e784fa9
Sripriya145 <124983968+Sripriya145@users.noreply.github.com>
2024-04-04 09:12:33 +0530

    Add files via upload
    yup

---
+++ WebFramework/src/main/java/com/webframework_project1/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.webframework_project1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ WebFramework/src/test/java/com/webframework_project1/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webframework_project1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
